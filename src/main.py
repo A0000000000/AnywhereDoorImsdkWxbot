2b386fb94ed243267cc6e8c58d75b95107fd64b1
maoyanluo <mengxin_zzl@outlook.com>
2025-03-02 17:15:17 +0800

    支持imsdk自动注册

--- src/main.py
+++ src/main.py
@@ -25,20 +25,23 @@ def register():
         self_prefix = constant.EMPTY_STR
     register_url = constant.TEMPLATE_REGISTER_URL % (host, port, prefix)
     while True:
-        res = requests.post(register_url, json={
-            constant.PARAMS_NAME: imsdk_name,
-            constant.PARAMS_HOST: self_host,
-            constant.PARAMS_PORT: self_port,
-            constant.PARAMS_PREFIX: self_prefix
-        }, headers={
-            constant.PARAMS_TOKEN: token,
-            constant.PARAMS_USERNAME: username
-        })
-        resp = json.loads(res.text)
-        if resp[constant.PARAMS_CODE] == -10:
+        try:
+            res = requests.post(register_url, json={
+                constant.PARAMS_NAME: imsdk_name,
+                constant.PARAMS_HOST: self_host,
+                constant.PARAMS_PORT: self_port,
+                constant.PARAMS_PREFIX: self_prefix
+            }, headers={
+                constant.PARAMS_TOKEN: token,
+                constant.PARAMS_USERNAME: username
+            })
+            resp = json.loads(res.text)
+            if resp[constant.PARAMS_CODE] == -10:
+                time.sleep(10)
+            else:
+                break
+        except Exception as e:
             time.sleep(10)
-        else:
-            break
 
 
 def main():
